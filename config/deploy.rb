c4abcd1463ee86d412c1a64e7bc0ccd296935c39
PeoneEr <peoneerko@yandex.ru>
2014-08-09 16:31:57 +0800

    added settings.yml to linked files

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,7 +12,7 @@ set :user, 'deploy'
 #role :app, domain
 #role :db, domain, :primary => true
 
-set :linked_files, %w{config/database.yml}
+set :linked_files, %w{config/database.yml config/settings.yml}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 set :whenever_identifier, ->{ "#{fetch(:application)}_#{fetch(:stage)}"  }
 
