bcca3e1d859bf9feeffb17bf2974f23fc1ffd978
PeoneEr <peoneerko@yandex.ru>
2014-09-01 11:11:29 +0700

    sa

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,6 +1,10 @@
-role :app, %w{deploy@192.168.0.105}
-role :web, %w{deploy@192.168.0.105}
-role :db,  %w{deploy@192.168.0.105}
+#role :app, %w{deploy@192.168.0.105}
+#role :web, %w{deploy@192.168.0.105}
+#role :db,  %w{deploy@192.168.0.105}
+
+role :app, %w{deploy@31.211.41.20}
+role :web, %w{deploy@31.211.41.20}
+role :db,  %w{deploy@31.211.41.20}
 
 set :stage, :production
-server '192.168.0.105', user: 'deploy', roles: %w{web app db}
+server '31.211.41.20', user: 'deploy', roles: %w{web app db}
