083e55d3567be9ca12bb0d1768eec633c42ae982
PeoneEr <peoneerko@yandex.ru>
2014-11-16 11:52:30 +0600

    dont send mails

--- config/schedule.rb
+++ config/schedule.rb
@@ -9,10 +9,10 @@ every 30.minutes do
 end
 
 
-every 1.hour do
-  rake "mailer:hour", :output => 'log/cron.log'
-end
+#every 1.hour do
+  #rake "mailer:hour", :output => 'log/cron.log'
+#end
 
-every :day, :at => '07:00am' do
-  rake "mailer:morning", :output => 'log/cron.log'
-end
+#every :day, :at => '07:00am' do
+  #rake "mailer:morning", :output => 'log/cron.log'
+#end
