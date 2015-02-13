32412c1e5b8ac26a2ee886282ca3d88be4e0cb3f
PeoneEr <peoneerko@yandex.ru>
2015-02-13 09:28:59 +0600

    нафиг почту

--- config/schedule.rb
+++ config/schedule.rb
@@ -10,9 +10,9 @@ end
 
 
 #every 1.hour do
-every '0 6,8,10,12,14,16,18 * * 1-5' do
-  rake "mailer:hour", :output => 'log/cron.log'
-end
+#every '0 6,8,10,12,14,16,18 * * 1-5' do
+  #rake "mailer:hour", :output => 'log/cron.log'
+#end
 
 #every :day, :at => '07:00am' do
   #rake "mailer:morning", :output => 'log/cron.log'
