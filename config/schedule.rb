336fd45414b4a7d3318a980088bd1d2ea13770a3
PeoneEr <peoneerko@yandex.ru>
2015-06-10 17:34:14 +0600

    update configuration

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,9 +1,12 @@
-every 6.hour do
-  rake "update:vk_and_facebook_stat", :output => "log/cron.log"
+every 7.hour do
+  rake "update:vk_and_facebook_stat"
 end
 
 every 30.minutes do
-  rake "update:rss", :output => "log/cron.log"
-  rake "update:stat_newest_entries", :output => "log/cron.log"
-  rake "update:twitter_stat", :output => "log/cron.log"
+  rake "update:rss"
+end
+
+every 40.minutes do
+  rake "update:stat_newest_entries"
+  rake "update:twitter_stat"
 end
