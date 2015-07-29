6dd87cccd5c0477371dd807e5b9538443f22548b
PeoneEr <peoneerko@yandex.ru>
2015-07-29 10:42:59 +0600

    comment schedule

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,16 +1,16 @@
-every 40.minutes do
-  rake "update:twitter_stat"
-end
+#every 40.minutes do
+  #rake "update:twitter_stat"
+#end
 
 every 1.hour do
-  rake "update:rss"
-  rake "update:stat_newest_entries"
+  #rake "update:rss"
+  #rake "update:stat_newest_entries"
   rake "mailer:hour"
 end
 
-every 7.hour do
-  rake "update:vk_and_facebook_stat"
-end
+#every 7.hour do
+  #rake "update:vk_and_facebook_stat"
+#end
 
 every :day, :at => '7:00am' do
   rake "mailer:morning"
